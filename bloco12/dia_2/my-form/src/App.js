a8c10efd59e937a15adf1f3ad2f23c1d4fb56921
Gabriel Essênio <gabriel.essenio@gmail.com>
2021-05-11 18:21:47 -0300

    add bloco 12

---
+++ bloco12/dia_2/my-form/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import DadosPessoais from './Componentes/DadosPessoais';
+
+function App() {
+  return (
+    <div className="App">
+      <DadosPessoais />
+    </div>
+  );
+}
+
+export default App;
